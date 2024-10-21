9095e7e613a090824b33aa8c236ac343e35fef06
Josh McKenney <jmckenney@Josh-McKenneys-MacBook-Pro.local>
2024-10-21 14:06:30 -0700

    debug function itself

--- netlify/functions/upload.js
+++ netlify/functions/upload.js
@@ -12,13 +12,13 @@ exports.handler = async function (event) {
     };
   }
 
-  const boundary = event.headers['content-type'].split('boundary=')[1];
-  const body = event.body.split(`--${boundary}`);
+  // const boundary = event.headers['content-type'].split('boundary=')[1];
+  // const body = event.body.split(`--${boundary}`);
 
-  const fileData = body[1].split('\r\n\r\n')[1].split('\r\n--')[0];
+  // const fileData = body[1].split('\r\n\r\n')[1].split('\r\n--')[0];
 
-  const uploadPath = path.join('/tmp', 'uploaded_file');
-  fs.writeFileSync(uploadPath, fileData, { encoding: 'binary' });
+  // const uploadPath = path.join('/tmp', 'uploaded_file');
+  // fs.writeFileSync(uploadPath, fileData, { encoding: 'binary' });
 
   return {
     statusCode: 200,
@@ -27,7 +27,7 @@ exports.handler = async function (event) {
     },
     body: JSON.stringify({
       message: 'File uploaded successfully',
-      path: uploadPath, // Include the file path in the response
+      // path: uploadPath, // Include the file path in the response
     }),
   };
 };
